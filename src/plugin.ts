793f64db7ee1d241ec8e4695577e448b8656ad68
Stepan <ste.martinek+git@gmail.com>
2023-12-01 20:36:13 +0100

    Allow initialize routes from webhooks. Added settings to be able to exclude routes initialization

--- src/plugin.ts
+++ src/plugin.ts
@@ -1,50 +1,30 @@
-import { FastifyPluginAsync, HTTPMethods } from 'fastify';
+import { FastifyPluginAsync } from 'fastify';
 import { fastifyPlugin } from 'fastify-plugin';
-import { Options, Paths } from './types.js';
+import { Options } from './types.js';
 import { registerComponents } from './components.js';
-import { parseParams } from './parseParams.js';
-import { createSecurityProcessors } from './createSecurityProcessors.js';
-import { createRouteSchema } from './routeSchema.js';
-import { defaultHandler } from './defaultOperationHandler.js';
+import { setupRoutes } from './setupRoutes.js';
 
 
 // define plugin using promises
-const myPluginAsync: FastifyPluginAsync<Options> = async (fastify, {openApiSpecification, securityHandlers, operationHandlers}) => {
+const myPluginAsync: FastifyPluginAsync<Options> = async (fastify, {openApiSpecification, securityHandlers, operationHandlers, settings}) => {
 
-  const {components, security: globalSecurity, paths} = openApiSpecification;
+  const {components, security: globalSecurity, paths, webhooks} = openApiSpecification;
   
   registerComponents(fastify, components);
-  
-  for (const [path, pathObject] of Object.entries(paths)) {
-    const {parameters, ...methods} = pathObject as Paths;
 
-    const params = parseParams(parameters ?? []);
+  if (settings?.initializePaths !== false && paths)
+  {
+    setupRoutes(fastify, {operationHandlers, paths, globalSecurity, securityHandlers}, false);
+  }
 
-    for (const [method, operation] of Object.entries(methods)) {
+  if (settings?.initializeWebhooks !== false && webhooks)
+  {
+    setupRoutes(fastify, {operationHandlers, paths: webhooks, globalSecurity, securityHandlers}, true);
+  }
 
-      const {parameters, operationId, requestBody, security, ...operationValues} = operation;
-
-      if (!operationId)
-      {
-        fastify.log.error(`${path} - ${method} is missing operationId! Will be skipped.`)
-        continue;
-      }
-
-      // Overrides any path params already defined
-      const operationParams = parseParams(parameters ?? [], structuredClone(params));
-
-      fastify.route({
-        method: method.toUpperCase() as HTTPMethods,
-        // fastify wants 'path/:param' instead of openapis 'path/{param}'
-        url: path.replace(/{(\w+)}/g, ":$1"),
-        handler: operationHandlers[operationId] ?? defaultHandler,
-        config: operationValues["x-fastify-config"], 
-        schema: createRouteSchema(operationParams, requestBody),
-        // Operation security overrides global security
-        preParsing: createSecurityProcessors(securityHandlers ?? {}, security ?? globalSecurity), 
-        
-      });
-    }
+  if (!paths && !webhooks)
+  {
+    fastify.log.error(`No paths or webhooks found in OpenAPI specification!`);
   }
 }
 
